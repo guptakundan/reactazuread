19aadeb1548c010197b5229877b09d98c4cc2f6a
Kundan Lal Gupta <kundagup@in.ibm.com>
2024-11-20 12:09:07 +0530

    init def val

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,7 +2,8 @@ const authConfig = {
     auth: {
         clientId: "5e1bdcae-3aa8-4926-b9d4-6ef83d389dd2",
         authority: `https://login.microsoftonline.com/5337e661-9904-4c3b-afbf-cca767cf5c5d`,
-        redirectUri: "http://localhost:3000", // Your redirect URI
+        redirectUri: "https://lively-tree-03ce93d0f.5.azurestaticapps.net"
+        //"http://localhost:3000", // Your redirect URI
     },
     //scopes: ["api://7243b58f-3928-4e60-b0ad-10ca1ac2d5bc/.default"],
     cache: {
