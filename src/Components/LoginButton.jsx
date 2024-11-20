67f6c685572378688eb1af1f6e91936a9b06f2fb
Kundan Lal Gupta <kundagup@in.ibm.com>
2024-11-20 11:39:09 +0530

    first commit

--- src/Components/LoginButton.jsx
+++ src/Components/LoginButton.jsx
@@ -3,7 +3,7 @@ import { useMsal } from '@azure/msal-react';
 const LoginButton = () => {
     const loginRequest = {
         // scopes: ["api://5e1bdcae-3aa8-4926-b9d4-6ef83d389dd2/.default"], //This is for API token from rest API
-        scopes: ["User.Read"] //this is for self web app token
+        scopes: ["User.Read"] //this is for self web app token.
     };
     const { accounts } = useMsal();
     //console.log(accounts);
